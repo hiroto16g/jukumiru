cd7b733aae91bde7abc5921af721d39ff3cf5c83
hiroto16g <43347783+hiroto16g@users.noreply.github.com>
2021-02-06 22:41:36 +0900

    メール送信実装

--- functions/index.js
+++ functions/index.js
@@ -41,11 +41,9 @@ exports.ssr = functions.https.onRequest(app)
 const CONFIG_SEND_MAIL = {
     "from": 'info@jukumiru.net',
     "user": 'info@jukumiru.net',
-    "pass": 'N32#IKOv',
-    //"user": 'sd1439801',
-    //"pass": 'N32#IKOv',
+    "pass": '#TZ7O9s4',
     "host": 'smtp21.gmoserver.jp',
-    "port": 587,
+    "port": 465,
 };
 // 送信プロトコル
 const SEND_CONFIG = {
@@ -64,23 +62,25 @@ const TRANSPOTER = nodemailer.createTransport(SEND_CONFIG);
     汎用処理
 ---------------------------------------------------- */
 // テンプレートHTMLの読み込み
-var read_html_template = async function (path, callback) {
-    fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
-        if (err) {
-            throw err;
-            callback(err);
-        }
-        else {
-            callback(null, html);
-        }
-    });
-};
+function read_html_template(path) {
+    return new Promise((resolve) => {
+        fs.readFile(path, 'utf-8', (err, data) => {
+            resolve(data)
+        })
+    })
+}
+// 改行あり用ヘルパー
+handlebars.registerHelper('breaklines', function(text) {
+    text = handlebars.Utils.escapeExpression(text);
+    text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+    return new handlebars.SafeString(text);
+});
 // メール送信
 async function send_mail(mail_data){
+    console.log(mail_data)
     try {
         // メール送信
         var result = await TRANSPOTER.sendMail(mail_data);
-        console.log(result)
         // 送信結果を返す
         return result;
     } catch (e) {
@@ -120,27 +120,26 @@ exports.send_introduction_mail = functions.https.onCall(async (data, context) =>
 async function create_introduction_mail_reuqest(params) {
 
     // マッピングパラメータ
-    var user_nm = params.user_nm;
-    var juku_nm = params.juku_nm;
     var to = params.to;
+    var juku_nm = params.juku_nm;
+    var user_nm = params.user_nm;
 
     // タイトル
-    var subject = 'お申し込みありがとうございました。';
+    var subject = '【塾ミル】お申し込みありがとうございました。';
 
     // 本文
     var html;
 
     // 本文作成
     // テンプレート読み出し
-    await read_html_template('./resource/mail_template/introduction/index.html', async function (err, template) {
-        // パラメータマッピング
-        var compiled_template = handlebars.compile(template);
-        var replacements = {
-            user_nm: user_nm,
-            juku_nm: juku_nm,
-        };
-        html = compiled_template(replacements);
-    });
+    var template = await read_html_template('./resource/mail_template/introduction/index.html');
+    // パラメータマッピング
+    var compiled_template = handlebars.compile(template);
+    var replacements = {
+        user_nm: user_nm,
+        juku_nm: juku_nm,
+    };
+    html = compiled_template(replacements);
 
     // QRコード生成
     // var qr_cd_url = await QRCode.toDataURL("https://blog.katsubemakito.net/nodejs/qrcode-generates");
@@ -173,13 +172,13 @@ async function create_introduction_mail_reuqest(params) {
 function create_introduction_mail_juku(params) {
 
     // マッピングパラメータ
+    var to = params.to;
+    var juku_nm = params.juku_nm;
     var course = params.course;
     var user_nm = params.user_nm;
     var school_grade = params.school_grade;
     var parent_nm = params.parent_nm;
     var parent_email = params.parent_email;
-    var juku_nm = params.juku_nm;
-    var to = params.to;
 
     // タイトル
     var subject = '【塾ミル】お申し込みのお知らせ'
@@ -192,18 +191,155 @@ function create_introduction_mail_juku(params) {
     いつもお世話になっております。
     株式会社塾ミルです。
 
-    塾ミルより、御社へのお申し込みがありました。
-    ------------------------------------------------
-    お申し込み内容：` + course + `
-    申込者氏名：` + user_nm + `
-    申込者学年：` + school_grade + `
-    保護者の方のお名前：` + parent_nm + `
-    保護者の方のメールアドレス：` + parent_email + `
-    ------------------------------------------------
+    塾ミルより、お客様へのお申し込みがありました。
+    ３営業以内でのご対応をお願いいたします。
+    
+    お申し込み内容：
+    ` + course + `
+    申込者氏名：
+    ` + user_nm + `
+    申込者学年：
+    ` + school_grade + `
+    保護者の方のお名前：
+    ` + parent_nm + `
+    保護者の方のメールアドレス：
+    ` + parent_email + `
+    
 
     以上、よろしくお願いいたします。
 
     `;
+
+    // メールデータ本体
+    var mail_data = {
+        // 送信元
+        from: CONFIG_SEND_MAIL.from,
+        // 送信先
+        to: to,
+        // タイトル
+        subject: subject,
+        // 本文
+        text: text,
+    };
+
+    return mail_data;
+}
+
+/* ----------------------------------------------------
+    問い合わせメール送信
+---------------------------------------------------- */
+exports.send_qa_mail = functions.https.onCall(async (data, context) => {
+
+    // 質問者パラメータ
+    var request_params = data.request;
+
+    // 塾用パラメータ
+    var juku_params = data.juku;
+
+    var result = {};
+
+    // 質問者にメール送信
+    var mail_data_request = await create_qa_mail_request(request_params);
+    result.request = await send_mail(mail_data_request);
+
+    // 塾にメール送信
+    var mail_data_juku = create_qa_mail_juku(juku_params);
+    result.juku = await send_mail(mail_data_juku);
+
+    return result;
+});
+
+/*
+    問い合わせメール作成：質問者用
+*/
+async function create_qa_mail_request(params) {
+
+    // マッピングパラメータ
+    var to = params.to;
+    var juku_nm = params.juku_nm;
+    var user_nm = params.user_nm;
+    var note = params.note;
+
+    // タイトル
+    var subject = '【塾ミル】お問い合わせ完了のお知らせ';
+
+    // 本文
+    var html;
+
+    // 本文作成
+    // テンプレート読み出し
+    var template = await read_html_template('./resource/mail_template/qa/index.html');
+    // パラメータマッピング
+    var compiled_template = handlebars.compile(template);
+    var replacements = {
+        user_nm: user_nm,
+        juku_nm: juku_nm,
+        note: note,
+    };
+    html = compiled_template(replacements);
+
+    // QRコード生成
+    // var qr_cd_url = await QRCode.toDataURL("https://blog.katsubemakito.net/nodejs/qrcode-generates");
+
+    // メールデータ本体
+    var mail_data = {
+        // 送信元
+        from: CONFIG_SEND_MAIL.from,
+        // 送信先
+        to: to,
+        // タイトル
+        subject: subject,
+        // 本文
+        html: html,
+        // 添付ファイル
+        // attachments: [
+        //     {
+        //         cid: 'qr_cd',
+        //         path: qr_cd_url,
+        //     },
+        // ],
+    };
+
+    return mail_data;
+}
+
+/*
+    問い合わせメール作成：塾用
+*/
+function create_qa_mail_juku(params) {
+
+    // マッピングパラメータ
+    var to = params.to;
+    var juku_nm = params.juku_nm;
+    var user_nm = params.user_nm;
+    var user_email = params.user_email;
+    var note = params.note;
+
+    // タイトル
+    var subject = '【塾ミル】お問い合わせのお知らせ'
+
+    // 本文
+    var text = `
+` + juku_nm + ` 様
+
+いつもお世話になっております。
+株式会社塾ミルです。
+
+塾ミルより、お客様へのお問い合わせがございました。
+ご対応をお願いいたします。
+
+お名前：
+` + user_nm + `
+メールアドレス：
+` + user_email + `
+お問い合わせ内容：
+` + note + `
+
+
+以上、よろしくお願いいたします。
+
+`;
+        
 
     // メールデータ本体
     var mail_data = {
--- store/juku.js
+++ store/juku.js
@@ -36,24 +36,51 @@ export const actions = {
 
         var call_params = {
             request : {
-                user_nm : input_params.user_nm,
-                juku_nm : input_params.juku_nm,
                 to : input_params.parent_email,
+                juku_nm : input_params.juku_nm,
+                user_nm : input_params.user_nm,
             },
             juku : {
+                to : input_params.juku_email,
+                juku_nm : input_params.juku_nm,
                 course : input_params.course,
                 user_nm : input_params.user_nm,
                 school_grade : input_params.school_grade,
                 parent_nm : input_params.parent_nm,
                 parent_email : input_params.parent_email,
-                juku_nm : input_params.juku_nm,
-                to : input_params.juku_email,
             },
         }
 
         // Function起動
         var cf_send_introduction_mail = firebase.functions().httpsCallable('send_introduction_mail')
         var response = await cf_send_introduction_mail(call_params)
+        console.log(response)
+    },
+    /* --------------------------------------------------
+        問い合わせメール送信
+    -------------------------------------------------- */
+    async send_qa_mail({commit}, input_params) {
+
+
+        var call_params = {
+            request : {
+                to : input_params.user_email,
+                juku_nm : input_params.juku_nm,
+                user_nm : input_params.user_nm,
+                note : input_params.note,
+            },
+            juku : {
+                to : input_params.juku_email,
+                juku_nm : input_params.juku_nm,
+                user_nm : input_params.user_nm,
+                user_email : input_params.user_email,
+                note : input_params.note,
+            },
+        }
+
+        // Function起動
+        var cf_send_qa_mail = firebase.functions().httpsCallable('send_qa_mail')
+        var response = await cf_send_qa_mail(call_params)
         console.log(response)
     },
 }
